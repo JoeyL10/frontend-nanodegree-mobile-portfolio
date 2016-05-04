dd1c2a13ed4605afcab8d459ded5a537ce90acc6
Joey <joe.laird1@gmail.com>
2016-05-04 06:03:57 -0400

    removed grunt responsive images and add imagemin

--- gruntfile.js
+++ gruntfile.js
@@ -1,50 +1,36 @@
 
-
+var mozjpeg = require('imagemin-mozjpeg');
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [{
-            name: 'sm',
-            width: '20%',
-            suffix: '',
-            quality: 20
-          },{
-            name: 'lg',
-            width: '50%',
-            suffix: '',
-            quality: 40
-          }]
-        },
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'img/',
-          dest: 'srcimg/',
-          cwd: 'views/images/',
-          dest: 'views/srcimages'
-        }]
-      }
-    },
-  });
+grunt.initConfig({
+  imagemin: {                          // Task        
+      options: {                       // Target options
+        optimizationLevel: 3,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [mozjpeg()]
+      },
+      // files: {                         // Dictionary of files
+      //   'dist/img.png': 'src/img.png', // 'destination': 'source'
+      //   'dist/img.jpg': 'src/img.jpg',
+      //   'dist/img.gif': 'src/img.gif'
+      // }
+   
+    dynamic: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'img/',                   // Src matches are relative to this path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'dist/'                // Destination path prefix
+      }]
+    }
+  }
+});
 
-    grunt.initConfig({
-      uglify: {
-        my_target: {
-          files: {
-            'views/js/main.min.js': ['views/js/main.js']
-          }
-        }
-      }
-    });
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['responsive_images']);
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['imagemin']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
 
 };
 
